2bffa0191cedb66257251879db84f3e969872ac2
Jeffry Franco <jeffryfranco304@gmail.com>
2026-05-20 22:42:39 -0500

    n

--- mongodb_test/tform/scripts/worker.sh
+++ mongodb_test/tform/scripts/worker.sh
@@ -4,10 +4,8 @@ yum install -y docker
 systemctl start docker
 systemctl enable docker
 
-# Login a ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 306970076789.dkr.ecr.us-east-1.amazonaws.com
 
-# Obtener IPs del Parameter Store
 RABBITMQ_IP=$(aws ssm get-parameter --name "/movies/rabbitmq_ip" --query 'Parameter.Value' --output text --region us-east-1)
 MONGO_IP=$(aws ssm get-parameter --name "/movies/mongo_ip" --query 'Parameter.Value' --output text --region us-east-1)
 
